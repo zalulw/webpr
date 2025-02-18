e176a7e8c6957f913ccb00e83e079cc34f673a57
zalulw <115061913+zalulw@users.noreply.github.com>
2025-02-18 14:21:49 +0100

    plants

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import _dirname from './util.rootpath.js';
+import __dirname from './util.rootpath.js';
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello cigany");
@@ -11,6 +12,20 @@ app.get("/index", (req, res) => {
     res.sendFile("./views.index.html", {root, _dirname})
 });
 
-app.listen(5800, () => {
+app.get("/:parameter", (req, res) => {
+    const param = req.params.parameter;
+    console.log(param);
+    res.send(param);
+})
+
+app.post("/", (req, res) => {
+    /*const name = req.body.name;
+    const age = req.body.age;*/
+    const {name, age} = req.body;
+    console.log(name, age);
+    res.json({name, age});
+})
+
+app.listen(3000, () => {
     console.log('Server runs on port 5800');
 }); //port beallitasa
