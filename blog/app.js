b6e82ce4f708cffe08dd6ca576deda7bfb662af8
zalulw <115061913+zalulw@users.noreply.github.com>
2025-05-28 07:38:18 +0200

    not tested

---
+++ blog/app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import db from "./data/database.js";
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    res.json(db.getUsers());
+});
+
+//#region posts 
+
+app.get('/posts', (req, res) => {
+    res.json(db.getPosts());
+});
+
+app.get('/posts/:id', (req, res) => {
+    const post = db.getPost(req.params.id);
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+app.post('/posts', (req, res) => {
+    const { author_id, title, category, content } = req.body;
+    if (!author_id || !title || !category || !content) {
+        return res.status(400).send('Missing required fields');
+    }
+    const postId = db.createPost(author_id, title, category, content);
+    res.status(201).json({ id: postId });
+});
+
+app.put('/posts/:id', (req, res) => {
+    const { title, category, content } = req.body;
+    if (!title || !category || !content) {
+        return res.status(400).send('Missing required fields');
+    }
+    db.updatePost(req.params.id, title, category, content);
+    res.sendStatus(204);
+});
+
+app.delete('/posts/:id', (req, res) => {
+    db.deletePost(req.params.id);
+    res.sendStatus(204);
+});
+//#endregion
+
+
+app.listen(8080, () => {
+    console.log("Server is running on :8080");
+});
